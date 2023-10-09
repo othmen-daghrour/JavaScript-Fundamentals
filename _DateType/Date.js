748dd0a00f5c7883c76d8e506a56ea5b206d8bb3
othmen.daghrour@hotmail.com <othmen-daghrour>
2023-10-09 14:46:39 +0100

    Adding Date Type to my repository

---
+++ _DateType/Date.js
@@ -0,0 +1,11 @@
+const today= new Date();
+
+const Current_Hour=today.getHours();
+const Current_Minutes=today.getMinutes();
+const Current_Second=today.getSeconds();
+
+console.log(today);
+
+console.log(Current_Hour);
+console.log(Current_Minutes);
+console.log(Current_Second);
